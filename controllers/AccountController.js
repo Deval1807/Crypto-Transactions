2cff88d1b3c41a40b1c5574550c719127fbd8c5d
Deval <deval135darji@gmail.com>
2024-08-31 21:46:43 +0530

    Add Account Service and Update Account Controller
    - Add the getTransactionByAddress service to fetch the transactions of a particular address
    - Use the getTransactionByAddress service in the Account controller to return the transactions

--- controllers/AccountController.js
+++ controllers/AccountController.js
@@ -1,6 +1,21 @@
-const GetTransactions = (req, res, next) => {
+const axios = require('axios');
+const { getTransactionsByAddress } = require('../services/AccountService');
+
+const GetTransactions = async (req, res, next) => {
+    
     const { address } = req.params;
-    return res.status(200).json({ "message": `All Transactions of address ${address}` })
+
+    try {
+        // Fetch all the transactions 
+        const transactions = await getTransactionsByAddress(address);
+        console.log(transactions);
+        return res.status(200).json(transactions);
+
+        
+    } catch (error) {
+        return res.status(400).json({ Error: `${error}` });
+    }
+
 }
 
 module.exports = { GetTransactions }
---
+++ services/AccountService.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+
+const getTransactionsByAddress = async(address) => {
+    const api_token = process.env.ETHERSCAN_API_TOKEN
+    const etherscan_url = process.env.ETHERSCAN_NORMAL_TRANSACTIONS_URL
+    try {
+        const response = await axios.get(etherscan_url, {
+            params: {
+                module: 'account',
+                action: 'txlist',
+                address: address,
+                startblock: 0,
+                endblock: 99999999,
+                page: 1,
+                offset: 10,
+                sort: 'asc',
+                apikey: api_token
+            }
+        });
+        return response.data.result;
+    } catch (error) {
+        console.error(error);        
+        return res.status(400).json({ Error: `${error}` });
+    }
+}
+
+module.exports = { getTransactionsByAddress }
