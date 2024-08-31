757f051fe546c1162e4b905e9ca9dfe182316513
Deval <deval135darji@gmail.com>
2024-08-31 18:59:09 +0530

    Add Database Connection Config

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 require('dotenv').config();
+const ConnectDB = require('./config/Database') 
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 8000;
+
+ConnectDB()
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
